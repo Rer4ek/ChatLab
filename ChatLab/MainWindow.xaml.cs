33eee1b59edf2e1b5bd5ae9e7d0c4d96c7ff61f0
Lambert <pborjakin@gmail.com>
2024-02-09 20:29:37 +0700

    Добавьте файлы проекта.

---
+++ ChatLab/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatLab
+{
+    public partial class MainWindow : Window
+    {
+        string historyPath = "history.txt";
+
+        void GoToEndBox(TextBox box)
+        {
+            box.Focus();
+            box.CaretIndex = box.Text.Length;
+            box.ScrollToEnd();
+        }
+
+        void PrintHistory()
+        {
+
+            ChatHistory.Text = string.Empty;
+            using (StreamReader reader = new StreamReader(historyPath))
+            {
+                string line;
+                while ((line =  reader.ReadLine()) != null)
+                {
+                    ChatHistory.Text += $"{line}\n";
+                }
+                reader.Close();
+            }
+            GoToEndBox(ChatHistory);
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PrintHistory();
+
+        }
+
+        private void ChatHistory_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+        private void SendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            if (Username.Text == string.Empty || Username.Text == "Пишите имя")
+            {
+                Username.Text = "Anonim";
+            }
+
+            using (StreamWriter writer = new StreamWriter(historyPath, true))
+            {
+                writer.WriteLineAsync($"{Username.Text} >> {Message.Text}    [{DateTime.Now}]");
+                writer.Close();
+            }
+
+            ChatHistory.Text +=  $"{Username.Text} >> {Message.Text}    [{DateTime.Now}]\n";
+            Message.Text = string.Empty;
+        }
+
+        private void Message_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SendMessage_Click(sender, e);
+            }
+        }
+    }
+}
---
+++ ChatLab/Window1.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Npgsql;
+using System.Data.Common;
+
+namespace ChatLab
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            DBConnection();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();     
+            Close();
+        }
+
+        static void DBConnection()
+        {
+            var connectionString = "Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=12345;";
+            var npgSqlConnection = new NpgsqlConnection(connectionString);
+            npgSqlConnection.Open();
+            Console.WriteLine("Succesful connection!");
+        }
+    }
+}
